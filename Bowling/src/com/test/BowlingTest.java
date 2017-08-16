7f39ab1ec32aabb919d47d0859cf6f23ca92e6f1
mghozzi <mokhtar.ghozzi-ext@kleegroup.com>
2017-08-16 17:21:11 +0200

    chnage files

--- Bowling/src/com/test/BowlingTest.java
+++ Bowling/src/com/test/BowlingTest.java
@@ -73,6 +73,17 @@ public class BowlingTest{
 		game.roll(5);
 	}
 
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	// Méthode permettant de renverser des quilles
 	private void rollMany(int n, int pins){
 		for(int i=0;i<n;i++){
